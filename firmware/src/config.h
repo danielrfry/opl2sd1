3f7dd4b06feb875da2d53e84c87e9769f9fc0f7f
Dan Fry <57286600+danielrfry@users.noreply.github.com>
2023-04-21 18:36:17 +0100

    Add build option for quad SD-1 mode.

--- firmware/src/config.h
+++ firmware/src/config.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#if !defined(OPL2SD1_QUAD)
+#define OPL2SD1_QUAD 0
+#endif
+
 #if !defined(OPL2SD1_STEREO)
 #define OPL2SD1_STEREO 0
 #endif
@@ -10,6 +14,10 @@
 // If stereo is not enabled, only PIN_CS_L is used
 #define PIN_CS_L 6
 #define PIN_CS_R 7
+
+// These pins are only used when quad SD-1 is enabled
+#define PIN_CS_L2 8
+#define PIN_CS_R2 9
 
 #define PIN_RST 4
 #define PIN_OE 5
